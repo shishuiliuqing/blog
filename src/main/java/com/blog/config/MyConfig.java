4bcdb666e083c1906b8e0a3d58fab6a0f32e8b21
shishuiliuqing <1410919730@qq.com>
2024-09-28 04:08:29 +0800

    映射文件配置

---
+++ src/main/java/com/blog/config/MyConfig.java
@@ -0,0 +1,25 @@
+package com.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.io.IOException;
+
+@Configuration
+public class MyConfig implements WebMvcConfigurer {
+    private File file = new File("src\\main\\resources\\static\\images");
+
+    //配置虚拟路径访问映射
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        try {
+            String imageAddress = file.getCanonicalPath().toString();
+            //映射图片保存地址
+            registry.addResourceHandler("/images/**").addResourceLocations("file:" + imageAddress + "\\");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
