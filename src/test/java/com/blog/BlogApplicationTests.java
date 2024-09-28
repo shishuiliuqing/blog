79cf22a0a6cf958abc61945c3a6a688ddd6f28cf
shishuiliuqing <1410919730@qq.com>
2024-09-28 20:29:23 +0800

    其他

---
+++ src/test/java/com/blog/BlogApplicationTests.java
@@ -0,0 +1,32 @@
+package com.blog;
+
+import com.blog.utils.ImageUtil;
+import com.blog.utils.JWTUtil;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() throws IOException {
+        ImageUtil imageUtil = new ImageUtil();
+//        System.out.println(imageUtil.a());
+    }
+
+    @Test
+    void a() {
+        String s = JWTUtil.generateJWT(1,"hjc");
+        System.out.println(s);
+    }
+
+    @Test
+    void b() {
+        Jws<Claims> jws = JWTUtil.parse("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJoamMiLCJleHAiOjE3Mjc1MjE3NDksInN1YiI6IkxvZ2luIiwiaXNzIjoiSldUX0pDIn0.WvE8Y3eAK363zk4bfTrEM-YQnt--HM_FEbmHDSBwYMw");
+        System.out.println(jws.getPayload().get("username"));
+    }
+}
